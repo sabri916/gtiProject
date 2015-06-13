bfda45b67e82778706e75fdc318277fb07f4fa0f
Sabri K <sbrkrs@gmail.com>
2015-06-13 13:40:36 +0400

    trying something else

--- Parent.java
+++ Parent.java
@@ -2,6 +2,6 @@ public class Parent{
 
 	public void display(){
 		System.out.println("I am a Parent");
-		System.out.println("Trying out something");
+		System.out.println("Trying out something else");
 	}
 }
