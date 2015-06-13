2c336d1096f4593ff38b83361179c08f854f1597
Sabri K <sbrkrs@gmail.com>
2015-06-13 12:17:53 +0400

    Child added

--- Child.java
+++ Child.java
@@ -2,5 +2,7 @@ public class Child extends Parent{
 
 	public void display(){
 		System.out.println("I am a Child");
+		System.out.print("This is what my Parent has to say: ");
+		super.display();
 	}
 }
